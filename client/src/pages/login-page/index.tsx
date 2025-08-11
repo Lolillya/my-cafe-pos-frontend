90a6b7cc4db0e25dadc8949d10bdd73360384f60
Lolillya <someonesusernam@gmail.com>
2025-08-12 01:32:04 +0800

    added login page

--- client/src/pages/login-page/index.tsx
+++ client/src/pages/login-page/index.tsx
@@ -1,11 +1,21 @@
 const LoginPage = () => {
   return (
-    <section>
-      <div>
-        <span>Login Page</span>
+    <section className="bg-bg1">
+      <div className="container flex items-center justify-center">
+        <div className="w-2/6 p-5 rounded-md flex flex-col gap-5">
+          <div className="flex flex-col gap-1">
+            <label>Username</label>
+            <input placeholder="Johndoe" />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label>Password</label>
+            <input type="password" placeholder="************" />
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
