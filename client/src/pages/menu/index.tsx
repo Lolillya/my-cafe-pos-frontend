57592398e5068af63d742270641019ecda2a5a75
Lolillya <someonesusernam@gmail.com>
2025-08-16 05:14:46 +0800

    adjust container padding and spacing

--- client/src/pages/menu/index.tsx
+++ client/src/pages/menu/index.tsx
@@ -1,12 +1,15 @@
 import { SearchCategory } from './_components/search-category';
 import { CategoryButtons } from './_components/category-buttons';
+import { ItemCardContainer } from './_components/item-card-wrapper';
 
 const MenuPage = () => {
   return (
-    <section className="bg-bg1">
-      <div className="container">
+    <section className="bg-bg1 ">
+      <div className="container gap-4 flex flex-col">
         <SearchCategory />
         <CategoryButtons />
+
+        <ItemCardContainer />
       </div>
     </section>
   );
